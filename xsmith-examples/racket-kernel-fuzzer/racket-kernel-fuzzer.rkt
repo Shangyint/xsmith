7c7a445269a199398abe82f56d45acd4842f224e
William G Hatch <william@hatch.uno>
2020-10-20 16:58:10 -0600

    fix my seconds->date fix... to actually use the fix

--- xsmith-examples/racket-kernel-fuzzer/racket-kernel-fuzzer.rkt
+++ xsmith-examples/racket-kernel-fuzzer/racket-kernel-fuzzer.rkt
@@ -637,9 +637,9 @@
           ;; Hopefully these bounds should work for any setup, as it's only a few hundred years in either direction.
           (define min-second -10000000000)
           (define max-second 10000000000)
-          (cond [(< x min-second) (seconds->date (modulo x min-second))]
-                [(> x max-second) (seconds->date (modulo x max-second))]
-                [else (seconds->date x)])
+          (cond [(< x min-second) (sd (modulo x min-second))]
+                [(> x max-second) (sd (modulo x max-second))]
+                [else (sd x)])
           )
         (define-values (safe-car)
           (λ (list fallback)
